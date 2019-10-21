b129d5673407461990c1275102c54914e691de86
SebastianLindfors <sebastianlindfors@gmail.com>
2019-10-21 16:11:08 +0200

    Added solution to Codewars Kata: 4K-Pyramid SLide Down

---
+++ Codewars/Java/4K-Pyramid Slide Down/src/PyramidSlideDown.java
@@ -0,0 +1,22 @@
+
+/** This class solves the Codewars Kata: Pyramid Slide Down
+ *  https://www.codewars.com/kata/551f23362ff852e2ab000037
+ *
+ *  Author: Sebastian Lindfors
+ */
+public class PyramidSlideDown {
+
+    public static int longestSlideDown(int[][] pyramid) {
+
+        for (int i = pyramid.length - 2; i >= 0; i--) {
+            for (int j = 0; j < pyramid[i].length; j++) {
+                pyramid[i][j] = pyramid[i][j] + Math.max(pyramid[i+1][j], pyramid[i+1][j+1]);
+            }
+        }
+        return pyramid[0][0];
+
+    }
+
+
+
+}
