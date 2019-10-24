c464da338a95cf5b9be3112ec25980a267a3a9b4
SebastianLindfors <sebastianlindfors@gmail.com>
2019-10-24 18:44:41 +0200

    Added solution to Codewars Kata: 4K-Binary Numbers Divisible by 5 for Java

--- Codewars/Java/4K-Regular Expression for binary numbers divisible by 5/src/DivisibleByFive.java
+++ Codewars/Java/4K-Regular Expression for binary numbers divisible by 5/src/DivisibleByFive.java
@@ -6,18 +6,7 @@ public class DivisibleByFive {
 
     public static Pattern pattern() {
 
-        System.out.println(Integer.toBinaryString(1000));
-        System.out.println(Integer.toBinaryString(1025));
-        System.out.println(Integer.toBinaryString(1500));
-        System.out.println(Integer.toBinaryString(5000));
-        System.out.println(Integer.toBinaryString(5025));
-        System.out.println(Integer.toBinaryString(6430));
-        System.out.println(Integer.toBinaryString(6435));
-        System.out.println(Integer.toBinaryString(7800));
-        System.out.println(Integer.toBinaryString(8005));
-        System.out.println(Integer.toBinaryString(10000));
-        // Partial solution:
-        return Pattern.compile("^0|(101(0)*)$");
+        return Pattern.compile("^(0|1(10)*(0|11)(01*01|01*00(10)*(0|11))*1)+$");
     }
 
 
