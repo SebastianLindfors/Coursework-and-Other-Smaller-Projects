19706975c272c90653700d0d81dede6dae97ba55
SebastianLindfors <sebastianlindfors@gmail.com>
2019-11-27 23:55:16 +0100

    Added solution to codewars kata 5K-Common Denominators for java

--- Codewars/Java/5K-CommonDenominators/src/CommonDenominator.java
+++ Codewars/Java/5K-CommonDenominators/src/CommonDenominator.java
@@ -1,21 +1,67 @@
 import java.util.HashMap;
 import java.util.Map;
 
-
+/** This class solves the Codewars Kata: Pyramid Slide Down
+ *  https://www.codewars.com/kata/551f23362ff852e2ab000037
+ *
+ *  Warning: This is a REALLY messy solution.
+ *  
+ *  Author: Sebastian Lindfors
+ */
 public class CommonDenominator {
     // your code
     public static String convertFrac(long[][] list) {
+
+        long[][] output = list;
+        for (int i = 0; i < output.length; i++) {
+            for (int j = 2; j <= Math.min((int) output[i][0] ,(int) output[i][1]); j++) {
+                if (output[i][0] % j == 0 && output[i][1] % j == 0) {
+                    output[i][0] /= j;
+                    output[i][1] /= j;
+                    j--;
+                }
+            }
+        }
+
         Map<Long, Integer> primes;
+        Map<Long, Integer> commonPrimes = new HashMap<>();
         for (int i = 0; i < list.length; i++) {
             System.out.print(i + ": ");
             primes = primeFactors(list[i][1]);
             for (long j : primes.keySet()) {
-                System.out.print(j + " " + primes.get(j) + " ");
+                if (commonPrimes.containsKey(j)) {
+                    if (primes.get(j) > commonPrimes.get(j)) {
+                        commonPrimes.put(j, primes.get(j));
+
+                    }
+                }
+                else {
+                    commonPrimes.put(j, primes.get(j));
+                }
+                System.out.print(j + " " + primes.get(j) + " | ");
             }
             System.out.println();
         }
 
-        return null;
+        int product = 1;
+        for (long key : commonPrimes.keySet()) {
+            product *= Math.pow(key,commonPrimes.get(key));
+        }
+        System.out.println("Factor: " + product);
+
+        StringBuilder outputString = new StringBuilder();
+        for (int i = 0; i < output.length; i++) {
+             long factor = product/output[i][1];
+
+             output[i][0] *= factor;
+             output[i][1] *= factor;
+            String fractionString = "(" + output[i][0] + "," + output[i][1] + ")";
+            System.out.println(fractionString);
+            outputString.append(fractionString);
+        }
+
+
+        return outputString.toString();
     }
 
     private static Map<Long, Integer> primeFactors(long n) {
--- Codewars/Java/5K-CommonDenominators/src/CommonDenominatorTesting.java
+++ Codewars/Java/5K-CommonDenominators/src/CommonDenominatorTesting.java
@@ -10,4 +10,11 @@ public class CommonDenominatorTesting {
         lst = new long[][] { {1, 2}, {1, 3}, {10, 40} };
         Assertions.assertEquals("(6,12)(4,12)(3,12)", CommonDenominator.convertFrac(lst));
     }
+    @Test
+    public void test_fractions2() throws Exception {
+        long[][] lst;
+        lst = new long[][] { {60, 240}, {45, 90}, {18, 72}, {34, 68}, {71, 142}, {71, 213}, {19, 38}, {55, 220} };
+        Assertions.assertEquals("(3,12)(6,12)(3,12)(6,12)(6,12)(4,12)(6,12)(3,12)", CommonDenominator.convertFrac(lst));
+
+    }
 }
